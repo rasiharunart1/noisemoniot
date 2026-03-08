e09fdf5a834483306093a67744d267fb1d58211c
rasiharunart1 <2211102080@ittelkom-pwt.ac.id>
2026-03-09 04:31:43 +0700

    resolve schedule

--- resources/views/devices/show.blade.php
+++ resources/views/devices/show.blade.php
@@ -506,13 +506,11 @@
                         @if($errors->hasAny(['start_time', 'end_time', 'interval_minutes', 'duration_seconds']))
                         <div class="p-3 rounded-xl bg-red-500/10 border border-red-500/20">
                             <ul class="space-y-1">
-                                @foreach($errors->only(['start_time', 'end_time', 'interval_minutes', 'duration_seconds']) as $fieldErrors)
-                                    @foreach((array)$fieldErrors as $error)
-                                    <li class="text-xs text-red-600 dark:text-red-400 flex items-center space-x-1.5">
-                                        <svg class="w-3 h-3 shrink-0" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd"/></svg>
-                                        <span>{{ $error }}</span>
-                                    </li>
-                                    @endforeach
+                                @foreach($errors->all() as $error)
+                                <li class="text-xs text-red-600 dark:text-red-400 flex items-center space-x-1.5">
+                                    <svg class="w-3 h-3 shrink-0" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd"/></svg>
+                                    <span>{{ $error }}</span>
+                                </li>
                                 @endforeach
                             </ul>
                         </div>
